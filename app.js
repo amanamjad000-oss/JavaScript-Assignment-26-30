4e8205016671979a828430843d4ac526c9f17fdd
Pakeeza Noor <amanamjad000@gmail.com>
2026-01-31 21:05:07 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+//Chapter 26-30(Q No 01)
+//var userInput = prompt("Please enter a positive number(e.g., 3.45214):");
+//var num = parseFloat(userInput);
+//if(num > 0) {
+  //  document.write("<h3> Number Anamlysis </h3>");
+    //document.write("<b> a. number: </b>" + num + "<br>");
+    //document.write("<b>b. round off value:</b> " + Math.round(num) + "<br>");
+    //document.write("<b>c. floor value:</b> " + Math.floor(num) + "<br>");
+    //document.write("<b>d. ceil value:</b> " + Math.ceil(num) + "<br>");
+//} else{
+  //  document.write("Please refresh and enter a valid positive number.")
+//}
+
+//Chapter 26-30(Q No 02)
+//var userInput = prompt("Please enter a negetive floating point number(e.g., -2.673):");
+//var num = parseFloat(userInput);
+//if(num > 0) {
+  //  document.write("<h3>Negetive Number Anamlysis </h3>");
+    //document.write("<b> a. number: </b>" + num + "<br>");
+    //document.write("<b>b. round off value:</b> " + Math.round(num) + "<br>");
+    //document.write("<b>c. floor value:</b> " + Math.floor(num) + "<br>");
+    //document.write("<b>d. ceil value:</b> " + Math.ceil(num) + "<br>");
+//} else{
+  //  document.write("Please refresh and enter a valid negetive number.")
+//}
+
+//Chapter 26-30(Q No 03)
+//var number = -4;
+//var absolute =Math.abs(number);
+//document.write("The absolute value of " + number + " is " + absolute);
+
+//Chapter 26-30(Q No 04)
+//var dice1 = Math.floor(Math.random() * 6) + 1;
+//var dice2 = Math.floor(Math.random() * 6) + 1;
+//document.write("random dice value:" + dice1 + "<br>");
+//document.write("random dice value:" + dice2);
+
+//Chapter 26-30(Q No 05)
+//var toss1 = Math.random() < 0.5 ? "Heads" : "Tails";
+//var toss2 = Math.random() < 0.5 ? "Heads" :"Tails";
+//document.write("2 random coin value:" + toss1 + "<br>");
+//document.write("1 random coin value:" + toss2);
+
+//Chapter 26-30(Q N o 06)
+//var randomNumber = Math.floor(Math.random() * 100) + 1;
+//document.write("random number between 1 and 100:" + randomNumber);
+
+//Chapter 26-30(Q No 07)
+//var userInput = prompt("Enter your weight in kilogram");
+//var weight = parseFloat(userInput);
+//document.write("The weight of user is" + weight + "kilogram");
+
+//Chapter 26-30(Q No 08)
+//var secretNumber = Math.floor(Math.random() * 10) + 1;
+//var userGuess = prompt("Guess the secret number between 1 and 10:");
+//if(parseInt(userGuess) === secretNumber) {
+  //alert("Congratulation! You duessed the secret number.");
+  //document.write("Awesome! The secret number was indeed" + secretNumber);
+//} else{
+  //alert("Try again! The secret number was" + secretNumber);
+  //document.write("Close, but no luck! The secret number was" + secretNumber);
+//}
+
+
+
+
+
+
